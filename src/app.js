df8161ea96334b884f5ceaf046f73ef7a095e9ac
Omkesh <omkeshjadhav.dev@gmail.com>
2025-10-05 16:58:44 +0530

    Routing and Advanced Routing

--- src/app.js
+++ src/app.js
@@ -4,20 +4,36 @@ const PORT = 7878
 
 app.get("/user", (req, res) => {
     console.log("GET user data");
+    console.log(req.query)
     res.send("User data received successfully.")
 })
+
+app.get("/user/:userId/:name", (req, res) => {
+    console.log("GET user data");
+    console.log(req.params)
+    res.send("User data received successfully.")
+})
+
+app.get(/^\/a(bc)?d$/, (req, res) => {
+    res.send({ firstName: "Omkesh", lastName: "Jadhav" });
+});
+
 app.post("/user", (req, res) => {
     console.log("Update user data");
-    res.send("New user data added successfully.")
+    res.send("New user created successfully")
 })
+
+
 app.patch("/user", (req, res) => {
     console.log("Update user data");
     res.send("User data updated successfully.")
 })
+
 app.delete("/user", (req, res) => {
     console.log("Delete user data");
     res.send("User data deleted successfully.")
 })
+
 
 app.listen(PORT, (req, res) => {
     console.log("Server is listening successfully!!!");
