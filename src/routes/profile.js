544f5151ed6d12107b596bd804963b79123a8594
Omkesh <omkeshjadhav.dev@gmail.com>
2025-10-17 22:15:17 +0530

    Update

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -14,29 +14,6 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
     }
 })
 
-// profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
-//     const data = req.body;
-//     const userId = req.params.userId
-
-//     try {
-//         const allowed_Updates = ["password", "image", "assets"]
-//         const isUpdateAllowed = Object.keys(data).every(key => allowed_Updates.includes(key))
-
-//         if (!isUpdateAllowed) {
-//             throw new Error("Update Not Allowed")
-//         } else if (data.skills.length > 5) {
-//             throw new Error("You can enter max 5 skills")
-//         }
-//         else {
-//             const user = await User.findByIdAndUpdate(userId, data, { runValidators: true });
-//             res.send(`User data for id ${userId} updated successfully!!`)
-//         }
-
-//     } catch (error) {
-//         res.status(500).send({ message: "Something went wrong", error: error.message })
-//     }
-// })
-
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     try {
         if (!editProfileValidator(req)) {
