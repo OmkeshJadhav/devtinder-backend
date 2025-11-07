8684ba2780b034e6797c3fd5d6c8fbd7303b602f
Omkesh <omkeshjadhav.dev@gmail.com>
2025-11-07 08:08:11 +0530

    Error Message updated for authentication

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
         // Read the token from req cookies
         const { token } = req.cookies;
         if (!token) {
-            throw new Error("Token is invalid!")
+            return res.status(401).send("You are not logged in. Please Login.")
         }
 
         // Validate the token
